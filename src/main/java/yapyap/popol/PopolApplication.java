ae113f9b24b5ddcb1801009911daa9e00262a2cd
paul.jeoung <kuni5458@naver.com>
2025-02-17 10:36:30 +0900

    first commit

---
+++ src/main/java/yapyap/popol/PopolApplication.java
@@ -0,0 +1,13 @@
+package yapyap.popol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yapyap/popol/PopolApplicationTests.java
@@ -0,0 +1,13 @@
+package yapyap.popol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
